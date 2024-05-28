f64ade96db388f46db623c95c6bc6099b82fe056
Sayandeep Paul <sayandeep.paul@anotechsolutions.com>
2024-05-28 15:34:26 +0530

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -16,18 +16,15 @@ const ChangePassword = require('../routes/ChangePassword/ChangePassword');
 const createNotification = require('../routes/Notification/AllUserNotification');
 const getNotification = require('../routes/Notification/AllUserNotification');
 const commission = require('../routes/Admin/commisionRoute');
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 const CreateAddress = require('./Admin/TRX-Address')
 const UpdateAddress = require('./Admin/TRX-Address')
 const getAddresses = require('./Admin/TRX-Address')
 const UPIAddress = require('./Admin/UPIAddress')
 const UpdateUPI = require('./Admin/UPIAddress')
 const Getid = require('./Admin/UPIAddress')
-////////////////////////////////////////////////////////////////////////////////////////////////////////
 const transaction = require('./Admin/TransactionHistoryRoute')
 const Savings = require('./wallet/SavingsAmount')
 const ShowSavings = require('./wallet/SavingsAmount')
-
 const trxresult = require('../TRXResult/TRXResult')
 const wingoresult = require('./wingo/wingoResultroute')
 const k3result = require('../K3Resut/K3ResultRoute')
@@ -46,7 +43,6 @@ router.use('/', ChangePassword);
 router.use('/', createNotification); 
 router.use('/', getNotification);
 router.use('/', commission);
-///////////////////////////////////////////////////////////
 router.use('/', CreateAddress)
 router.use('/', UpdateAddress)
 router.use('/', getAddresses)
@@ -60,43 +56,5 @@ router.use('/',ShowSavings)
 router.use('/',trxresult)
 router.use('/',wingoresult)
 router.use('/',k3result)
-
-
-const registerRoute = require("../routes/auth/registerRoute");
-const loginRoute = require("../routes/auth/loginRoute");
-const logoutRoute = require("../routes/auth/logoutRoute");
-const walletRoute = require("../routes/wallet/walletRoute");
-const betRoute = require("../routes/wingo/wingoRoutes");
-const couponRoutes = require("../routes/common/coupenCodeRoute");
-const todaysJoinee = require("../routes/users/userDetailsRoute");
-const userStats = require('../routes/users/userStatisticsRoute')
-const transactions = require("../routes/wallet/TodaysRecharge");
-const userBalance = require("../routes/Admin/UserBalance");
-const withdraw = require("../routes/Admin/withdrawRoute");
-const ChangePassword = require("../routes/ChangePassword/ChangePassword");
-const createNotification = require("../routes/Notification/AllUserNotification");
-const getNotification = require("../routes/Notification/AllUserNotification");
-const commission = require("../routes/Admin/commisionRoute");
-const savings = require("../routes/wallet/SavingsAmount");
-const getSavings = require("../routes/wallet/SavingsAmount");
-
-router.use("/", registerRoute);
-router.use("/", loginRoute);
-router.use("/", logoutRoute);
-router.use("/", walletRoute);
-router.use("/", betRoute);
-router.use("/", couponRoutes);
-router.use("/", todaysJoinee);
-router.use("/", transactions);
-router.use("/", userBalance);
-router.use("/", withdraw);
-router.use("/", ChangePassword);
-router.use("/", createNotification);
-router.use("/", getNotification);
-router.use("/", commission);
-router.use("/", savings);
-router.use("/", getSavings);
-router.use("/",userStats)
-
 
 module.exports = router;
