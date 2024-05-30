f8850091580a17d9b6c9813fc1b40c91058838c4
Techydeveloper12 <dheeraj4567yadav@gmail.com>
2024-05-30 19:35:52 +0530

    jkhj

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,25 +4,33 @@ const User = require('../models/userModel');
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.cookies.token; // Get token from cookies
+        const token = req.cookies.token;
         console.log(`Token: ${token}`);
+        
+        if (!token) {
+            return res.status(401).send({ error: 'No token provided.' });
+        }
+        
         const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
         console.log(`Decoded: ${JSON.stringify(decoded)}`);
+
         const user = await User.findById(decoded.id);
         console.log(`User: ${JSON.stringify(user)}`);
 
         if (!user) {
-            throw new Error();
+            throw new Error('User not found.');
         }
 
-        req.token = token;
+        // Add the token to the user object in the request
+        user.token = token; // Store the token
         req.user = user;
+
         next();
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
         res.status(401).send({ error: 'Please authenticate.' });
     }
 }
+
 
 module.exports = auth;
